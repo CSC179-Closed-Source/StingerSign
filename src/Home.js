f7976fc396c0bedbf814ba2cfc884bfcb1ef6dd1
Juan <juanambriz@csus.edu>
2022-04-06 06:50:29 -0700

    Changes to home, navbar. Home.css.js added images styling edits Navbar was edited for Stinger Sign, padding fixed to fit menu

--- src/Home.js
+++ src/Home.js
@@ -6,17 +6,17 @@ function Home() {
   return (
       <div className="home">
            <div className ="top-section">
-            <h1>Every day someone signs something that means everything.</h1>
+            <h1 style={{textAlign: "center"}}>Signing made easy!</h1>
             </div>
             <div className="intro">
-                <h1> Welcome to FORM sign</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                <h1 style={{textAlign: "center"}}> Welcome to Stinger-Sign</h1>
+                <p> You can use this website to Sign documents, manage signed documents, view your document history and easily upload documents to be signed. </p>
                 </div>
             
           
           </div>
-     
-
+     // Edited line 9,12,13
+          
     
   );
 }
