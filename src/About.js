a7d849ee7ba089ba4de9c282e14555e74433bf22
Juan <juanambriz@csus.edu>
2022-04-06 17:49:39 -0700

    Made About Page Multiple edits to CSS About Added Images

--- src/About.js
+++ src/About.js
@@ -1,19 +1,33 @@
 import React from "react";
+import './About.css'
 
 function About() {
  
   return (
       <div className="about">
-           <div className ="top-section">
-            <h1>Every day someone signs something that means everything.</h1>
+
+           <div className ="aboutTop-section">
+            <h1 style={{textAlign: "center"}}>Created with love by Team Closed Source</h1>
+            <p style={{textAlign: "center"}}></p>
             </div>
-            <div className="intro">
-                <h1> Welcome to FORM sign</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-                </div>
+
+            <div className="aboutIntro">
+                <h1> About Stinger Sign</h1>
+                <p>  This website was created for CSC 179.</p>
+                <p> The purpose is to easily access, manage and share documents that need signatures.</p>
+            </div>
             
-          
-          </div>
+
+            <div className="aboutTeam">
+                <h1> Team Closed Source</h1>
+                <p> Juan Ambriz</p>
+                <p> Nataly Avalos </p>
+                <p> Harish Kandaswamy </p>
+                <p> Abdul Tareq Khaliq </p>
+                <p> Harjap Singh </p>
+                <p> Keith Valdes </p>
+            </div>          
+      </div>
      
 
     
